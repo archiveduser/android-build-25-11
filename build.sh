067a86e11f26b2fdffa347bbe1c9e486f79a7ef2
kamino <lishichenjs@163.com>
2021-12-31 22:05:05 +0800

    update build script

--- build.sh
+++ build.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
-
+echo "TASK START"
 if test -z "$REPO"
 then
     echo "Get Project From $REPO"
-    rm /root/project -rf
-    git clone $REPO /root/project
+    rm /repo -rf
+    git clone $REPO /repo
 else
-    if [ ! -d "/root/project/gradlew" ]; then
-        echo "Get Project From /root/project"
+    if [ ! -d "/repo/gradlew" ]; then
+        echo "Get Project From /repo"
     else
         echo "Can't Find Android Studio Project"
+        echo "TASK EXIT"
+        exit
     fi
 fi
 
+echo "START BUILD"
+
+cd /repo
 
 chmod +x ./gradlew
 ./gradlew
 ./gradlew app:assembleRelease
+
+echo "BUILD FINISHED"
